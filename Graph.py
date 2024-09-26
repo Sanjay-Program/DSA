6df1695630540ca331ce2a4f1ea697445dbc6d45
Sanjay-Program <sanjaymurugadosscsbs02@gmail.com>
2024-09-26 17:25:19 +0530

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -1,21 +1,14 @@
-def create_adjacency_matrix(num_nodes, edges):
-    # Initialize a num_nodes x num_nodes matrix with zeros
+#This is a graph using adjacency matrix
+def create_adjacency_matrix(num_nodes, edgees):
     adjacency_matrix = [[0] * num_nodes for _ in range(num_nodes)]
-    
-    # Fill the matrix based on the edges
     for (start, end) in edges:
-        adjacency_matrix[start][end] = 1  # For directed graph
-        # adjacency_matrix[end][start] = 1  # Uncomment for undirected graph
-
+        adjacency_matrix[start][end] = 1 
     return adjacency_matrix
 
 def display_matrix(matrix):
     for row in matrix:
         print(" ".join(map(str, row)))
-
-# Example usage
-num_nodes = 5  # Number of nodes in the graph
-edges = [(0, 1), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (3, 4)]  # List of edges
-
+num_nodes = 5  
+edges = [(0, 1), (0, 4), (1, 2), (1, 3), (1, 4), (2, 3), (3, 4)]  
 adjacency_matrix = create_adjacency_matrix(num_nodes, edges)
 display_matrix(adjacency_matrix)
